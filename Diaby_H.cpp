2218fba87fc7221eb3d08219df168e7a751c3af3
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2024-09-16 16:25:57 -0400

    Skibdii poo poo pee pee doo doo yeah
    LALALALLALALLALALLA

---
+++ Diaby_H.cpp
@@ -0,0 +1,54 @@
+// Halimatou Diaby
+//Lecture 2
+//9/16/24
+
+
+
+
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main() {
+    string skibidi = "Big skibidi sigma likes to walk";    //This is the string thingy
+    cout<< skibidi;
+
+
+    int Num1 = 7;                                            // This is the int thingy
+    cout << Num1 << endl;
+
+    float FloatNum1 = 4.6;
+    cout << FloatNum1 << endl;                               // This is the  float thingy
+
+
+    double DoubleNum1 = 16.27;
+    cout << DoubleNum1 <<endl;
+
+
+
+    char CharThngy = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;             
+}
